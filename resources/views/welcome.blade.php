4e79900c0b5125348d9f25347f254fe2893eb31b
SanderWeideGC <sander.weide@student.graafschapcollege.nl>
2020-11-17 12:52:26 +0100

    changed welcome.blade.php content for server testing

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Tennis vereneging DTV
                 </div>
 
                 <div class="links">
