34b1d7cb9269f54e76a4116a0a6bec416b7e2600
SanderWeideGC <sander.weide@student.graafschapcollege.nl>
2020-11-19 12:51:02 +0100

    Added basic template structure

---
+++ resources/views/frontend/header.blade.php
@@ -0,0 +1,86 @@
+<!doctype html>
+<html class=no-js lang="">
+<meta http-equiv="content-type" content="text/html;charset=utf-8"/>
+<head>
+    <meta charset=utf-8>
+    <meta name=description content="">
+    <meta name=viewport content="width=device-width, initial-scale=1">
+    <title>soccer club</title>
+    <link rel="shortcut icon" href=favicon.ico>
+    <link rel=stylesheet href=vendor.css>
+    <link rel=stylesheet href=style.css>
+    <link rel=stylesheet type=text/css href=css/layerslider.css>
+    <script src=js/vendor/modernizr.js></script>
+</head>
+<body><!--[if lt IE 10]>
+<p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade
+    your browser</a> to improve your experience.</p>
+<![endif]-->
+<div class=wrapper>
+    <header class=header-main>
+        <div class=header-upper>
+            <div class=container>
+                <div class=row>
+                    <ul>
+                        <li><a href="#">Signup/login</a></li>
+                        <li><a href=shopcart.html><i class="fa fa-shopping-cart"></i> <span>cart(<span
+                                class=cartitems>0</span>)</span></a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="header-lower clearfix">
+            <div class=container>
+                <div class=row><h1 class=logo><a href=index-2.html><img src=images/logo.png alt=image></a></h1>
+
+                    <div class=menubar>
+                        <nav class=navbar>
+                            <div class=nav-wrapper>
+                                <div class=navbar-header>
+                                    <button type=button class=navbar-toggle><span class=sr-only>Toggle navigation</span>
+                                        <span class=icon-bar></span></button>
+                                </div>
+                                <div class=nav-menu>
+                                    <ul class="nav navbar-nav menu-bar">
+                                        <li><a href=/ class=active>Home <span></span> <span></span>
+                                            <span></span> <span></span></a></li>
+                                        <li><a href=about>about <span></span> <span></span> <span></span>
+                                            <span></span></a></li>
+                                        <li><a>gallery <span></span> <span></span> <span></span> <span></span></a>
+                                            <ul class=sub-menu>
+                                                <li><a href=gallerypage01.html>masonry</a></li>
+                                                <li><a href=gallery02.html>gallery column two</a></li>
+                                                <li><a href=gallery03.html>gallery column 03</a></li>
+                                            </ul>
+                                        </li>
+                                        <li><a href=blog.html>blog <span></span> <span></span> <span></span>
+                                            <span></span></a></li>
+                                        <li><a href=bookTicket.html>book Tickets <span></span> <span></span>
+                                            <span></span> <span></span></a></li>
+                                        <li><a href=shop.html>shop <span></span> <span></span> <span></span>
+                                            <span></span></a></li>
+                                        <li><a href=contact.html>contact <span></span> <span></span> <span></span>
+                                            <span></span></a></li>
+                                        <li><a>error <span></span> <span></span> <span></span> <span></span></a>
+                                            <ul class=sub-menu>
+                                                <li><a href=400.html>400 page</a></li>
+                                                <li><a href=401.html>401 page</a></li>
+                                                <li><a href=403.html>403 page</a></li>
+                                                <li><a href=404.html>404 page</a></li>
+                                                <li><a href=500.html>500 page</a></li>
+                                                <li><a href=503.html>503 page</a></li>
+                                            </ul>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </nav>
+                    </div>
+                    <div class=social><a href=https://www.facebook.com/templatespoint.net class=facebook><i
+                            class="fa fa-facebook"></i></a> <a href=https://twitter.com/itobuztech class=twitter><i
+                            class="fa fa-twitter"></i></a> <a href=https://www.behance.net/ class=behance><i
+                            class="fa fa-behance"></i></a></div>
+                </div>
+            </div>
+        </div>
+    </header>
