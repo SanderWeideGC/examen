94ff279d657d249da316748b5fda4717936bd218
SanderWeideGC <sander.weide@student.graafschapcollege.nl>
2020-11-24 16:18:36 +0100

    crud functions in all controllers

--- app/Http/Controllers/MatchesController.php
+++ app/Http/Controllers/MatchesController.php
@@ -7,5 +7,38 @@ use Illuminate\Http\Request;
 
 class MatchesController extends Controller
 {
-    //
+    public function index()
+    {
+        # code...
+    }
+
+    public function show($id)
+    {
+        # code...
+    }
+
+    public function create()
+    {
+        # code...
+    }
+
+    public function store()
+    {
+        # code...
+    }
+
+    public function edit()
+    {
+        # code...
+    }
+
+    public function update()
+    {
+        # code...
+    }
+
+    public function destroy()
+    {
+        # code...
+    }
 }
--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -10,4 +10,34 @@ class UsersController extends Controller
     public function index() {
         return view('backend.users');
     }
+
+    public function show($id)
+    {
+        # code...
+    }
+
+    public function create()
+    {
+        # code...
+    }
+
+    public function store()
+    {
+        # code...
+    }
+
+    public function edit()
+    {
+        # code...
+    }
+
+    public function update()
+    {
+        # code...
+    }
+
+    public function destroy()
+    {
+        # code...
+    }
 }
