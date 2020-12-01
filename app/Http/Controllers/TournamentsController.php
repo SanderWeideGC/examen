f9635d4ff0d3d35c7464439a0dfa5a367df796ee
SanderWeideGC <sander.weide@student.graafschapcollege.nl>
2020-12-01 13:53:19 +0100

    .

--- app/Http/Controllers/TournamentsController.php
+++ app/Http/Controllers/TournamentsController.php
@@ -10,4 +10,34 @@ class TournamentsController extends Controller
     public function index() {
         return view('backend.tournaments');
     }
+
+    public function show($id)
+    {
+        # code...
+    }
+
+    public function create()
+    {
+        # code...
+    }
+
+    public function store()
+    {
+        # code...
+    }
+
+    public function edit()
+    {
+        # code...
+    }
+
+    public function update()
+    {
+        # code...
+    }
+
+    public function destroy()
+    {
+        # code...
+    }
 }
