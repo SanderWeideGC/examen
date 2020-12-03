385c7f667fc95f691f02bedcbdbb3889d35eadaf
Ben-gc <47322123+Ben-gc@users.noreply.github.com>
2020-12-03 13:36:55 +0100

    Logo's aangepast naar het opgeleverde logo

--- resources/views/frontend/header.blade.php
+++ resources/views/frontend/header.blade.php
@@ -33,7 +33,7 @@
         </div>
         <div class="header-lower clearfix">
             <div class=container>
-                <div class=row><h1 class=logo><a href=/><img src=images/logo.png alt=image></a></h1>
+                <div class=row><h1 class=logo><a href=/><img src="images/logo-wit.png" alt="DTV logo" style="max-width:180%"></a></h1>
 
                     <div class=menubar>
                         <nav class=navbar>
