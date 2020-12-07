d2e9dd21eda0010d3ff2d27f916056a0e870d6af
SanderWeideGC <sander.weide@student.graafschapcollege.nl>
2020-12-07 16:49:37 +0100

    removed double functions in tournaments

--- app/Http/Controllers/TournamentsController.php
+++ app/Http/Controllers/TournamentsController.php
@@ -98,33 +98,4 @@ class TournamentsController extends Controller
         ];
     }
 
-    public function show($id)
-    {
-        # code...
-    }
-
-    public function create()
-    {
-        # code...
-    }
-
-    public function store()
-    {
-        # code...
-    }
-
-    public function edit()
-    {
-        # code...
-    }
-
-    public function update()
-    {
-        # code...
-    }
-
-    public function destroy()
-    {
-        # code...
-    }
 }
